0d6ff3e061ebb417b4749698c3054364e0ff423c
최근영 <ought119@gmail.com>
2023-07-21 13:51:46 +0900

    problem_f_2.py 문제 해결

--- Python/관통 PJT/1 주차/관통 Ver2/aladin/problem_f_2.py
+++ Python/관통 PJT/1 주차/관통 Ver2/aladin/problem_f_2.py
@@ -2,10 +2,19 @@ import json
 
 
 def sorted_cs_books_by_price(books, categories):
-    # 여기에 코드를 작성합니다.  
-    pass
+    id_number = 0
+    for v in categories:
+        if v["name"] == "컴퓨터 공학":
+            id_number = v["id"]
+    
+    cate_books = []
+    
+    for book in books:
+        if id_number in book["categoryId"]:
+            cate_books.append(book["title"])
 
-
+    return cate_books
+    
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     books_json = open('data/books.json', encoding='utf-8')
