f4e3aed002dd1736d2cfb6a22f3751387b26ab84
최근영 <ought119@gmail.com>
2023-07-21 14:52:21 +0900

    problem_f_1.py spotify_문제 해결

--- Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_f_1.py
+++ Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_f_1.py
@@ -7,11 +7,31 @@ import json
 from pprint import pprint
 
 
-def get_popular():
-    # 여기에 코드를 작성합니다.
-    pass
+def get_popular(artists):
+
+    art_dict = {}
+    for artist in artists:
+        art_id = artist["id"]
+        art_id_popular = open(f'data/artists/{art_id}.json', encoding="utf-8")
+        art_id_popular = json.load(art_id_popular)
+        art_dict[art_id] = (art_id_popular["name"], art_id_popular["followers"] , art_id_popular["uri"].split(":"))
+    
+    return_list = []
+
+    for art_detail in art_dict.values():
+        if art_detail[1]["total"] >= 5000000 and art_detail[1]["total"] < 10000000:
+            return_artist = {}
+            return_artist["name"] = art_detail[0]
+            return_artist["followers"] = art_detail[1]["total"]
+            return_list.append(return_artist)
+
+    return return_list
+
 
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
-    pprint(get_popular())
+    artists_json = open('data/artists.json', encoding='utf-8')
+    artists_list = json.load(artists_json)
+
+    pprint(get_popular(artists_list))
