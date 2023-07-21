c2687a78019c63c3220e9b2a39340bb560bd490b
최근영 <ought119@gmail.com>
2023-07-21 14:49:54 +0900

    problem_e.py spotify_문제 해결

--- Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_e.py
+++ Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_e.py
@@ -1,9 +1,25 @@
 import json
-
+from pprint import pprint as print
 
 def dec_artists(artists):
-    # 여기에 코드를 작성합니다.
-    pass
+
+    art_dict = {}
+    for artist in artists:
+        art_id = artist["id"]
+        art_id_popular = open(f'data/artists/{art_id}.json', encoding="utf-8")
+        art_id_popular = json.load(art_id_popular)
+        art_dict[art_id] = (art_id_popular["name"], art_id_popular["followers"] , art_id_popular["uri"].split(":"))
+    
+    return_list = []
+
+    for art_detail in art_dict.values():
+        if art_detail[1]["total"] > 10000000:
+            return_artist = {}
+            return_artist["name"] = art_detail[0]
+            return_artist["uri-id"] = art_detail[2][-1]
+            return_list.append(return_artist)
+
+    return return_list
 
 
 # 아래의 코드는 수정하지 않습니다.
