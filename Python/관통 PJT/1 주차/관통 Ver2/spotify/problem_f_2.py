4047f3f13894560f10e9b8390111d7444b5e5ab7
최근영 <ought119@gmail.com>
2023-07-21 14:59:16 +0900

    problem_f_2.py spotify_문제 해결

--- Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_f_2.py
+++ Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_f_2.py
@@ -6,11 +6,27 @@ import json
 from pprint import pprint
 
 
-def acoustic_artists():
-    # 여기에 코드를 작성합니다.
-    pass
+def acoustic_artists(artists, genres):
+    genre_id = 0
+    for genre in genres:
+        if genre["name"] == "acoustic":
+            genre_id = genre["id"]
+    
+    artists_name = []
+
+    for artist in artists:
+        if genre_id in artist["genres_ids"]:
+            artists_name.append(artist["name"])
+    
+    return artists_name
 
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
-    pprint(acoustic_artists())
+    genres_data = open("data/genres.json", encoding="utf-8")
+    genres_data = json.load(genres_data)
+
+    artists_data = open("data/artists.json", encoding="utf-8")
+    artists_data = json.load(artists_data)
+
+    pprint(acoustic_artists(artists_data, genres_data))
