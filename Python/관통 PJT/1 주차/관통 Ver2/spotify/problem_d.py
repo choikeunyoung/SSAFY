165f211a40c4b6ce44d95713563817014c24d6f0
최근영 <ought119@gmail.com>
2023-07-21 14:19:17 +0900

    problem_d.py spotify_문제 해결

--- Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_d.py
+++ Python/관통 PJT/1 주차/관통 Ver2/spotify/problem_d.py
@@ -2,9 +2,23 @@ import json
 
 
 def max_revenue(artists):
-    # 여기에 코드를 작성합니다.
-    pass
+    art_dict = {}
+    for artist in artists:
+        art_id = artist["id"]
+        art_id_popular = open(f'data/artists/{art_id}.json', encoding="utf-8")
+        art_id_popular = json.load(art_id_popular)
 
+        art_dict[art_id] = (art_id_popular["name"], art_id_popular["popularity"])
+
+    max_id = 0
+    max_popular = 0
+
+    for k, v in art_dict.items():
+        if v[1] > max_popular:
+            max_id = k
+            max_popular = v[1]
+    
+    return art_dict[max_id][0]
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
