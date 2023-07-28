5224480343003301ec1faafe08f28921e4651f64
최근영 <ought119@gmail.com>
2023-07-28 16:06:58 +0900

    관통ver2/aladin/problem_b.py

--- Python/관통 PJT/2 주차/관통ver2/aladin/problem_b.py
+++ Python/관통 PJT/2 주차/관통ver2/aladin/problem_b.py
@@ -1,14 +1,31 @@
 import requests
-from pprint import pprint
+from pprint import pprint as print
+import json
+# 검색 요청 API 확인해야함
+URL = 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx'
 
+params = {
+    'ttbkey': 'ttbwwaall26261516002',
+    'Query': '파울로 코엘료',
+    'QueryType': 'Author',
+    'MaxResults' : 20,
+    'start' : 1,
+    'SearchTarget' : 'Book',
+    'output' : 'js',
+    'Version' : '20131101'
+}
 
-def best_review_books():
-    # 여기에 코드를 작성합니다.  
-    pass
+response = requests.get(URL, params=params).json()
+def best_review_books(res_infos):
+    res_list = []
+    for res_info in res_infos:
+        if res_info["customerReviewRank"] >= 9:
+            res_list.append(res_info)
 
+    return res_list
 
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
 
-    pprint(best_review_books())
+    print(best_review_books(response['item']))
