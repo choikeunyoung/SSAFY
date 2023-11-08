fc706862be35b8a895f33e7081358b2dbd80f303
choikeunyoung <ought119@gmail.com>
2023-11-08 17:59:55 +0900

    1108 실습

---
+++ Vue/1108/vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Parent />
+  </div>
+</template>
+
+<script setup>
+import Parent from './Parent.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/1108/vue-project/src/ParentChild.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ myMsg }}
+    <ParentGrandChildVue :my-msg="myMsg" />
+  </div>
+</template>
+
+<script setup>
+import ParentGrandChildVue from "./ParentGrandChild.vue"
+// defineProps(['myMsg'])
+const test = defineProps({
+  myMsg : String,
+  // myMsg : {
+  //   type : String,
+  //   required : true,
+  // }
+})
+console.log(test.myMsg)
+</script>
+
+<style scoped>
+
+</style>
